5e202589c545378d22da3050f1a6b10f18c7a51e
Doni Setiawan <donisetiawan.050@gmail.com>
2020-02-27 14:09:07 +0700

    Creating our own middleware

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
+const errorHandlers = require('./middleware/errorhandlers');
+const log = require('./middleware/log');
 
 const app = express();
 const port = 3000;
+
+app.use(log.logger);
+app.use(express.static(`${__dirname}/static`));
 
 app.get('/', routes.index);
 app.get('/login', routes.login);
 app.post('/login', routes.loginProcess);
 app.get('/chat', routes.chat);
+app.get('/error', (req, res, next) => {
+  next(new Error('A contrived error'));
+});
+
+app.use(errorHandlers.error);
+app.use(errorHandlers.notFound);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
