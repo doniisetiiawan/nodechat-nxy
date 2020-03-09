81027bc84e9b4417b7576595960d4a55105c28d0
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-09 17:27:12 +0700

    Finishing Socket.IO

--- socket.io/index.js
+++ socket.io/index.js
@@ -4,6 +4,18 @@ import expressSession from 'express-session';
 import redis from 'redis';
 import redisAdapter from 'socket.io-redis';
 import config from '../config';
+import {
+  addUser,
+  removeUserFromRoom,
+  getChat,
+  addChat,
+  getRooms,
+  addRoom,
+  addUserToRoom,
+  getUsersinRoom,
+} from '../redis/chat';
+import { User, Chat, Room } from '../redis/models';
+import { error } from '../middleware/log';
 
 let io = require('socket.io');
 
@@ -30,24 +42,134 @@ const socketAuth = (socket, next) => {
 
   redisSession.get(sid, (err, session) => {
     if (session.isAuthenticated) {
-      socket.user = session.user;
-      socket.sid = sid;
+      socket.request.user = session.passport.user;
+      socket.request.sid = sid;
+      addUser(
+        session.passport.user.id,
+        session.passport.user.displayName,
+        session.passport.user.provider,
+      );
       return next();
     }
     return next(new Error('Not Authenticated'));
   });
+};
 
-  return next(new Error('Nothing Defined'));
+const removeFromRoom = (socket, room) => {
+  socket.leave(room);
+  removeUserFromRoom(socket.request.user.id, room);
+  socket.broadcast
+    .to(room)
+    .emit(
+      'RemoveUser',
+      User(
+        socket.request.user.id,
+        socket.request.user.displayName,
+        socket.request.user.provider,
+      ),
+    );
+};
+
+const removeAllRooms = (socket, cb) => {
+  const current = socket.rooms;
+  const len = Object.keys(current).length;
+  let i = 0;
+  current.forEach((element) => {
+    if (element !== socket.id) {
+      removeFromRoom(socket, element);
+    }
+    i++;
+    if (i === len) cb();
+  });
 };
 
 const socketConnection = (socket) => {
-  socket.on('GetMe', () => {});
-  socket.on('GetUser', (room) => {});
-  socket.on('GetChat', (data) => {});
-  socket.on('AddChat', (chat) => {});
-  socket.on('GetRoom', () => {});
-  socket.on('AddRoom', (r) => {});
-  socket.on('disconnect', () => {});
+  socket.on('GetMe', () => {
+    socket.emit(
+      'GetMe',
+      User(
+        socket.request.user.id,
+        socket.request.user.displayName,
+        socket.request.user.provider,
+      ),
+    );
+  });
+
+  socket.on('GetUser', (room) => {
+    const usersP = getUsersinRoom(room.room);
+    usersP.done((users) => {
+      socket.emit('GetUser', users);
+    });
+  });
+
+  socket.on('GetChat', (data) => {
+    getChat(data.room, (chats) => {
+      const retArray = [];
+      let len = chats.length;
+      chats.forEach((c) => {
+        try {
+          retArray.push(JSON.parse(c));
+        } catch (e) {
+          error(e.message);
+        }
+        len--;
+        if (len === 0) socket.emit('GetChat', retArray);
+      });
+    });
+  });
+
+  socket.on('AddChat', (chat) => {
+    const newChat = Chat(
+      chat.message,
+      chat.room,
+      User(
+        socket.request.user.id,
+        socket.request.user.displayName,
+        socket.request.user.provider,
+      ),
+    );
+    addChat(newChat);
+    socket.broadcast.to(chat.room).emit('AddChat', newChat);
+    socket.emit('AddChat', newChat);
+  });
+
+  socket.on('GetRoom', () => {
+    getRooms((rooms) => {
+      const retArray = [];
+      let len = rooms.length;
+      rooms.forEach((r) => {
+        retArray.push(Room(r));
+        len--;
+        if (len === 0) socket.emit('GetRoom', retArray);
+      });
+    });
+  });
+
+  socket.on('AddRoom', (r) => {
+    const room = r.name;
+    removeAllRooms(socket, () => {
+      if (room !== '') {
+        socket.join(room);
+        addRoom(room);
+        socket.broadcast.emit('AddRoom', Room(room));
+        socket.broadcast
+          .to(room)
+          .emit(
+            'AddUser',
+            User(
+              socket.request.user.id,
+              socket.request.user.displayName,
+              socket.request.user.provider,
+            ),
+          );
+        addUserToRoom(socket.request.user.id, room);
+      }
+    });
+  });
+
+  socket.on('disconnect', () => {
+    removeAllRooms(socket, () => {});
+  });
 };
 
 const startIo = (server) => {
